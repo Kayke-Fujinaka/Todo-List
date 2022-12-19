802fc0aac3bf246ea45d26fdd8a26e7d23e85463
Kayke-Fujinaka <kaykealvesfujinaka@gmail.com>
2022-12-19 16:56:37 -0300

    styles: create color theme

---
+++ src/styles/theme/colors.ts
@@ -0,0 +1,12 @@
+export const colors = {
+  black: "#000000",
+  white: "#ffffff",
+  blue: "#273FAD",
+  green: "#3FAD27",
+  red: "#ad273f",
+  background: "#EBEBEB",
+  shapes: "#FFFFFF",
+  text_title: "#3D3D4D",
+  text: "#1C1C29",
+  text_light: "#A09CB1",
+};
